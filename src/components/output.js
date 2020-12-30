5af2ad2631ada0c902b511cb254a0a6cde65a5ff
SAHIL PRASAD <39877110+sahilp007@users.noreply.github.com>
2020-12-30 17:42:25 +0530

    Added Base project

---
+++ src/components/output.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Output = (props) => {
+	const {paragraphs, tag, includeHtml} = props;
+	return (
+		<div className="output">
+			{includeHtml === "Yes" ? (
+				<p>{paragraphs.map((sentence) => `<${tag}>${sentence}</${tag}>`)}</p>
+			) : (
+				<p>{paragraphs.map((sentence) => sentence)}</p>
+			)}
+		</div>
+	);
+};
+
+
+export default Output;
